134d150f377424cd3a0030b42306304599d3760e
lucageorge <new_luca@yahoo.com>
2025-05-24 20:33:05 +0300

    dynamic filepath & refactor

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
+import sys
 from stats import get_num_words, get_ascii_distribution, prepare_report_data 
 
 def get_book_text(file_path):
     with open(file_path, "r") as file:
         return file.read()
 
-def main():
-    file_path = "books/frankenstein.txt" 
-    book_text = get_book_text(file_path)
+def print_report(file_path, book_text):
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {file_path}...")
     print("----------- Word Count ----------")
@@ -17,5 +16,12 @@ def main():
             print(f"{d['char']}: {d['num']}")
     print("============= END ===============")
 
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    file_path = sys.argv[1]
+    book_text = get_book_text(file_path)
+    print_report(file_path, book_text)
 
 main()
